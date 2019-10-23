2d4c912a9eab573510525bb6042e1397887288ad
iguxa <tsybykov@bk.ru>
2019-10-23 10:26:06 +0300

    fix locale bug

---
+++ Modules/Core/Http/Middleware/LocaleSessionRedirectMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iguxa
+ * Date: 23.10.19
+ * Time: 10:23
+ */
+
+namespace Modules\Core\Http\Middleware;
+use Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect;
+
+class LocaleSessionRedirectMiddleware extends LocaleSessionRedirect
+{
+
+}
--- Modules/Shorturl/Resources/lang/en/shorturls.php
+++ Modules/Shorturl/Resources/lang/en/shorturls.php
@@ -9,16 +9,26 @@ return [
         'shorturls' => 'Кароткие ссылки',
         'create shorturl' => 'Create a shorturl',
         'edit shorturl' => 'Edit a shorturl',
+        'watch'=>'Посмостреть',
+        'create'=>'Создать',
+        'data'=>'Данные',
     ],
     'button' => [
         'create shorturl' => 'Create a shorturl',
+        'new-shorturls'=>'Создать короткую ссылку'
     ],
     'table' => [
+        'status'=>'Статус',
+        'link'=>'Ссылка',
+        'redirect'=>'Перенаправление',
+        'counter'=>'Счетчик',
+
     ],
     'form' => [
         'state'=>[
             'true'=>'активно',
             'false'=>'не активно',
+            'state'=>'Статус',
         ],
         'redirect'=>'Перенаправление',
         'description'=>'Описание',
