2be76d959c00bd2f28b581470260af52679637bb
Viktor <vvairuss@mail.ru>
2019-12-01 10:02:18 +0300

    Add translation

--- Modules/Services/Resources/lang/en/services.php
+++ Modules/Services/Resources/lang/en/services.php
@@ -1,24 +1,31 @@
 <?php
 
 return [
-    'list resource' => 'List services',
-    'create resource' => 'Create services',
+    'list resource' => 'Список микросервисов',
+    'create resource' => 'Добавить микросервис',
     'edit resource' => 'Edit services',
     'destroy resource' => 'Destroy services',
     'title' => [
-        'services' => 'Services',
+        'services' => 'Микросервисы',
         'create services' => 'Create a services',
         'edit services' => 'Edit a services',
+        'data' => 'Данные',
     ],
     'button' => [
         'create services' => 'Create a services',
+        'new-services' => 'Добавить микросервис',
     ],
     'table' => [
     ],
     'form' => [
+        'title' => 'Название',
+        'description' => 'Описание',
+        'state' => 'Включено',
+        'api_url' => 'URL для автообновления',
+        'related_services' => 'Связанные сервисы',
     ],
     'messages' => [
     ],
     'validation' => [
-    ],
+    ]
 ];
